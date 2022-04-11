d7e77f2d8edc631b3a3553372b32281f67365087
swj <159632_@naver.com>
2022-04-11 15:52:31 +0900

    Add : 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
