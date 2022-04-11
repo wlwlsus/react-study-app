783c14d43203ea116ef0a0c4c57fe946e24cd4e7
swj <159632_@naver.com>
2022-04-11 16:58:37 +0900

    Add : URI Parameter

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <div>Detail</div>;
+  const { id } = useParams();
+  const [detailImg, setImage] = useState("");
+  const getMovies = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setImage(json.data.movie.large_cover_image);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      <h1>Detail</h1>
+      <img alt="" src={detailImg}></img>
+    </div>
+  );
 }
 
 export default Detail;
